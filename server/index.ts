46e44e11b901c3b8205edf34d53d86b70fb2b206
menghaoyu <menghaoyu2002@gmail.com>
2022-06-16 19:36:02 -0400

    added basic folder structure

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: './config/.env' });
 
 const app: Express = express();
 const port = process.env.PORT;

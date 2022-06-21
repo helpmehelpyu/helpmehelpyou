5b556055b9b590820db01b4813ed1b32ffaa2a7d
menghaoyu <menghaoyu2002@gmail.com>
2022-06-21 17:26:44 -0400

    changed typeorm to connect with database uri instead of setting up all the params

--- server/database/dataSource.ts
+++ server/database/dataSource.ts
@@ -1,16 +1,9 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { User } from '../models/User';
-import { Link } from '../models/Link';
-import { Media } from '../models/Media';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.POSTGRES_PORT!, 10),
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD!,
-    database: process.env.POSTGRES_DATABASE,
+    url: process.env.DATABASE_URL,
     ssl:
         process.env.NODE_ENV === 'production'
             ? { rejectUnauthorized: false }

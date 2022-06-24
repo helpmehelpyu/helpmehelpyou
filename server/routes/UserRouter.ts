6d5bb986cf70cff79aff8f75fe608ccf4dc102ee
menghaoyu <menghaoyu2002@gmail.com>
2022-06-24 18:34:02 -0400

    added field validation for user creation

--- server/routes/UserRouter.ts
+++ server/routes/UserRouter.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import userController = require('../controllers/UserController');
 
 const router = Router();
 
-router.post('/signup');
+router.post(
+    '/signup',
+    body('firstName').trim().isLength({ min: 1 }).isAlpha().escape(),
+    body('lastName').trim().isLength({ min: 1 }).isAlpha().escape(),
+    body('email').isEmail().normalizeEmail().escape(),
+    body('password').isLength({ min: 6 }).isStrongPassword().escape(),
+    body('phoneNumber').optional().isMobilePhone('any').escape(),
+    userController.register
+);
 
-router.post('/login');
+router.post('/login', userController.register);
 
 router.post('/logout');
 

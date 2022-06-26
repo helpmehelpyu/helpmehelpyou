295c1e1c85bb1560e90aef4cfbbeb864f2ea1ad0
menghaoyu <menghaoyu2002@gmail.com>
2022-06-25 21:30:06 -0400

    Updated Register response to be uniform across all errors

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { validationResult } from 'express-validator';
+import { ValidationError, validationResult } from 'express-validator';
 import userService = require('../services/UserService');
 
 export const login = async (req: Request, res: Response) => {
@@ -14,7 +14,7 @@ export const login = async (req: Request, res: Response) => {
         });
     }
 
-    res.status(200).json({ auth_token: token });
+    res.status(200).json({ authToken: token });
 };
 
 export const register = async (req: Request, res: Response) => {
@@ -22,16 +22,27 @@ export const register = async (req: Request, res: Response) => {
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({
+                errors: errors.array().map((err: ValidationError) => {
+                    type: 'ValidationError';
+                    message: err.msg;
+                    value: err.value;
+                    location: err.location;
+                }),
+            });
         }
 
         const newUser = await userService.createUser(req.body);
         res.status(201).json({ userId: newUser.id });
     } catch (err: any) {
         res.status(400).json({
-            type: 'DuplicateEmailError',
-            message:
-                'A User with this account already exists, please log in instead',
+            errors: [
+                {
+                    type: 'DuplicateEmailError',
+                    message:
+                        'A User with this account already exists, please log in instead',
+                },
+            ],
         });
     }
 };

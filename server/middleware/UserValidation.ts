86b3de6e6b988b753a0fa975ca37f04971a7bc11
menghaoyu <menghaoyu2002@gmail.com>
2022-07-04 20:41:36 -0400

    Update User Registration to handle when phone number is null

--- server/middleware/UserValidation.ts
+++ server/middleware/UserValidation.ts
@@ -1,64 +1,68 @@
 import { body } from 'express-validator';
 
 export const validateUpdatedProperties = [
-  body('firstName')
-    .optional()
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage('First name must be at least one character long')
-    .isAlpha()
-    .withMessage('First name can only include alphabet characters')
-    .escape(),
-  body('lastName')
-    .optional()
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage('Last name needs to be at least one character')
-    .isAlpha()
-    .withMessage('Last name can only include alphabet characters')
-    .escape(),
-  body('email')
-    .optional()
-    .isEmail()
-    .withMessage('Invalid Email')
-    .normalizeEmail()
-    .escape(),
-  body('phoneNumber')
-    .optional()
-    .isMobilePhone('any')
-    .withMessage('Invalid Phone Number')
-    .escape(),
+    body('firstName')
+        .optional()
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('First name must be at least one character long')
+        .isAlpha('en-US', { ignore: ' ' })
+        .withMessage('First name can only include alphabet characters')
+        .escape(),
+    body('lastName')
+        .optional()
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Last name needs to be at least one character')
+        .isAlpha('en-US', { ignore: ' ' })
+        .withMessage('Last name can only include alphabet characters')
+        .escape(),
+    body('email')
+        .optional()
+        .isEmail()
+        .withMessage('Invalid Email')
+        .normalizeEmail()
+        .escape(),
+    body('phoneNumber')
+        .optional()
+        .isMobilePhone('any')
+        .withMessage('Invalid Phone Number')
+        .escape(),
 ];
 
 export const validateUserProperties = [
-  body('firstName')
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage('First name must be at least one character long')
-    .isAlpha()
-    .withMessage('First name can only include alphabet characters')
-    .escape(),
-  body('lastName')
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage('Last name needs to be at least one character')
-    .isAlpha()
-    .withMessage('Last name can only include alphabet characters')
-    .escape(),
-  body('email')
-    .isEmail()
-    .withMessage('Invalid Email')
-    .normalizeEmail()
-    .escape(),
-  body('password')
-    .isLength({ min: 6 })
-    .withMessage('Password needs to be at least 6 characters')
-    .isStrongPassword()
-    .withMessage('Password is not strong enough')
-    .escape(),
-  body('phoneNumber')
-    .optional()
-    .isMobilePhone('any')
-    .withMessage('Invalid Phone Number')
-    .escape(),
+    body('firstName')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('First name must be at least one character long')
+        .bail()
+        .isAlpha('en-US', { ignore: ' ' })
+        .withMessage('First name can only include alphabet characters')
+        .bail()
+        .escape(),
+    body('lastName')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Last name needs to be at least one character')
+        .bail()
+        .isAlpha('en-US', { ignore: ' ' })
+        .withMessage('Last name can only include alphabet characters')
+        .bail()
+        .escape(),
+    body('email')
+        .isEmail()
+        .withMessage('Invalid Email')
+        .bail()
+        .normalizeEmail()
+        .escape(),
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password needs to be at least 6 characters')
+        .bail(),
+    body('phoneNumber')
+        .optional()
+        .isMobilePhone('any')
+        .withMessage('Invalid Phone Number')
+        .bail()
+        .escape(),
 ];

845955faff2841c602abc827e632f7d75329c033
menghaoyu <menghaoyu2002@gmail.com>
2022-07-04 20:42:52 -0400

    Add logic for user registration

--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,5 +1,9 @@
 import UserRegistrationForm from '../components/UserRegistrationForm';
 
 export default function Register() {
-  return <UserRegistrationForm></UserRegistrationForm>;
+  return (
+    <div className="rounded flex justify-center items-center h-screen bg-slate-100">
+      <UserRegistrationForm></UserRegistrationForm>
+    </div>
+  );
 }

fa8f878c7b64e768bddbface0578d0ba467f17c5
Menghao Yu <menghaoyu2002@gmail.com>
2022-07-16 14:00:33 -0400

    Add Login Page

---
+++ client/pages/login.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '../components/LoginForm';
+
+export default function Login() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-white">
+      <LoginForm></LoginForm>
+    </div>
+  );
+}

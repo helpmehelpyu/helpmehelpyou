760d69c7d355e87b02e4b5140eb3abf919a05f74
Menghao Yu <menghaoyu2002@gmail.com>
2022-07-19 18:44:49 -0400

    Add basic upload page

---
+++ client/pages/upload.tsx
@@ -0,0 +1,9 @@
+import UploadMediaForm from '../components/UploadMediaForm';
+
+export default function Upload() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-white">
+      <UploadMediaForm></UploadMediaForm>
+    </div>
+  );
+}

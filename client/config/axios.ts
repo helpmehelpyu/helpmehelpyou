57ca9cb787d3cd83c26352347b2d9d20c47bf941
Menghao Yu <menghaoyu2002@gmail.com>
2022-07-20 21:53:31 -0400

    Updated axios requests to not send cookies automatically
    instead manually send cookies when needed

--- client/config/axios.ts
+++ client/config/axios.ts
@@ -1,11 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   validateStatus: function (status) {
     return status < 500;
   },
-  withCredentials: true,
 });
 
 export default instance;

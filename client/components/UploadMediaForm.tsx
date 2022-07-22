daeeb87eca6dc21a11564836dfda536ed1a09343
Menghao Yu <menghaoyu2002@gmail.com>
2022-07-22 18:09:15 -0400

    Bold UploadMediaForm h1

--- client/components/UploadMediaForm.tsx
+++ client/components/UploadMediaForm.tsx
@@ -1,18 +1,18 @@
-import axios from "../config/axios";
-import { FormEvent, useState } from "react";
-import { getAuthCookie } from "../auth/auth";
-import FloatingLabelInput from "./FloatingLabelField";
+import axios from '../config/axios';
+import { FormEvent, useState } from 'react';
+import { getAuthCookie } from '../auth/auth';
+import FloatingLabelInput from './FloatingLabelField';
 
 export default function UploadMediaForm() {
-  const [titleErrors, setTitleErrors] = useState("");
-  const [descriptionErrors, setDescriptionErrors] = useState("");
-  const [fileErrors, setFileErrors] = useState("");
+  const [titleErrors, setTitleErrors] = useState('');
+  const [descriptionErrors, setDescriptionErrors] = useState('');
+  const [fileErrors, setFileErrors] = useState('');
   const [canSubmit, setCanSubmit] = useState(true);
 
   const clearErrors = () => {
-    setTitleErrors("");
-    setDescriptionErrors("");
-    setFileErrors("");
+    setTitleErrors('');
+    setDescriptionErrors('');
+    setFileErrors('');
   };
 
   const uploadMedia = async (event: FormEvent) => {
@@ -22,23 +22,23 @@ export default function UploadMediaForm() {
     setCanSubmit(false);
 
     const mediaData = new FormData(event.target as HTMLFormElement);
-    const response = await axios.post("/media", mediaData, {
+    const response = await axios.post('/media', mediaData, {
       headers: {
-        Authorization: "Bearer " + getAuthCookie(),
+        Authorization: 'Bearer ' + getAuthCookie(),
       },
     });
 
     clearErrors();
     if (response.status === 201) {
-      document.location.href = "/media/" + response.data.mediaId;
+      document.location.href = '/media/' + response.data.mediaId;
     } else {
       setCanSubmit(true);
       setFileErrors(
-        (response.data.fileError && response.data.fileError.message) || ""
+        (response.data.fileError && response.data.fileError.message) || ''
       );
 
       for (const error of response.data.validationErrors) {
-        if (error.param === "title") {
+        if (error.param === 'title') {
           setTitleErrors(error.msg);
         } else {
           setDescriptionErrors(error.msg);
@@ -49,17 +49,17 @@ export default function UploadMediaForm() {
 
   return (
     <div className="rounded bg-white p-5 border-2 w-min-1/3">
-      <h1 className="p-1 m-2 text-2xl">Upload</h1>
+      <h1 className="p-1 m-2 text-3xl font-bold">Upload</h1>
       <form onSubmit={uploadMedia} encType="multipart/form-data" noValidate>
         <FloatingLabelInput
-          type={"text"}
+          type={'text'}
           isRequired={true}
           setValue={() => {}}
           placeholder="Title"
         ></FloatingLabelInput>
         <p className="text-red-500 text-sm mx-2 px-1">{titleErrors}</p>
         <FloatingLabelInput
-          type={"text"}
+          type={'text'}
           isRequired={false}
           setValue={() => {}}
           placeholder="Description"

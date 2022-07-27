dc7f5345bf3d3be6da3ed6a34764983f8aec1476
Menghao Yu <menghaoyu2002@gmail.com>
2022-07-26 23:03:37 -0400

    Refactor single quotes to dboule quotes in auth.ts

--- client/auth/auth.ts
+++ client/auth/auth.ts
@@ -1,16 +1,20 @@
-import Cookies from "js-cookie";
+import Cookies from 'js-cookie';
 
 export function setAuthCookie(authToken: string) {
-  Cookies.set("auth_token", authToken, {
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+  Cookies.set('auth_token', authToken, {
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'Strict',
   });
 }
 
 export function getAuthCookie(): string | undefined {
-  return Cookies.get("auth_token");
+  return Cookies.get('auth_token');
+}
+
+export function isAuthenticated(): boolean {
+  return Cookies.get('auth_token') !== undefined;
 }
 
 export function logout() {
-  Cookies.remove("auth_token");
+  Cookies.remove('auth_token');
 }

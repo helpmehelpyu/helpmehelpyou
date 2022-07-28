d7255b080504c369958a26fb956a56ef17b217cf
Menghao Yu <menghaoyu2002@gmail.com>
2022-07-27 22:47:26 -0400

    Add Preliminary pagination navigation

--- client/components/MediaPagination.tsx
+++ client/components/MediaPagination.tsx
@@ -1,12 +1,18 @@
 import { MediaResult } from '../types/MediaResult';
 import { WorkSample } from '../types/WorkSample';
 import Image from 'next/image';
+import { useState } from 'react';
+import { MediaPaginationNav } from './MediaPaginationNav';
 
 interface Props {
   media: MediaResult[] | WorkSample[];
 }
 
 export default function MediaPagination({ media }: Props) {
+  const itemsPerPage = 25;
+  const totalPages = Math.ceil(media.length / itemsPerPage);
+  const [currentPageNumber, setCurrentPageNumber] = useState(0);
+
   if (media.length === 0) {
     return (
       <h1 className="w-full text-center text-4xl font-light p-16">
@@ -15,18 +21,23 @@ export default function MediaPagination({ media }: Props) {
     );
   }
 
-  const items = media.map((val) => (
-    <li key={val.id} className="flex-[0_0_16.3%]">
-      <Image
-        src={val.source.toString()}
-        objectFit="cover"
-        height={5000}
-        width={5000}
-        className="rounded-2xl aspect-square scale-75 md2:scale-100"
-        alt=""
-      ></Image>
-    </li>
-  ));
+  const items = media
+    .slice(
+      currentPageNumber * itemsPerPage,
+      (currentPageNumber + 1) * itemsPerPage
+    )
+    .map((val) => (
+      <li key={val.id} className="flex-[0_0_16.3%] select-none">
+        <Image
+          src={val.source.toString()}
+          objectFit="cover"
+          height={5000}
+          width={5000}
+          className="rounded-2xl aspect-square scale-75 md2:scale-100"
+          alt=""
+        ></Image>
+      </li>
+    ));
 
   const fillerElements: JSX.Element[] = [];
   for (let i = 0; i < ((-media.length % 5) + 5) % 5; i++) {
@@ -41,6 +52,11 @@ export default function MediaPagination({ media }: Props) {
         {items}
         {fillerElements}
       </ul>
+      <MediaPaginationNav
+        currentPageNumber={currentPageNumber}
+        totalPages={totalPages}
+        setCurrentPageNumber={setCurrentPageNumber}
+      ></MediaPaginationNav>
     </div>
   );
 }

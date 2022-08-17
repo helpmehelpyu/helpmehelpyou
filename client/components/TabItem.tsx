461cecc67d2e94258745e10b274de0916cc6d1a8
Menghao Yu <menghaoyu2002@gmail.com>
2022-08-16 22:20:32 -0400

    Add + button to add more elements

--- client/components/TabItem.tsx
+++ client/components/TabItem.tsx
@@ -8,16 +8,23 @@ interface Props {
 
 export default function TabItem({ tab, setSelectedTab, isSelected }: Props) {
   return (
-    <li>
+    <li className="whitespace-nowrap">
       <button
         onClick={() => setSelectedTab(tab)}
         className={
-          'text-lg hover:underline p-2 rounded hover:bg-gray-200 duration-300' +
-          (isSelected ? ' text-cyan-500 border border-cyan-500 font-bold' : '')
+          'text-lg hover:underline p-2 hover:bg-gray-200 duration-300' +
+          (isSelected
+            ? ' text-cyan-500 border rounded-l border-cyan-500 font-bold'
+            : ' rounded')
         }
       >
         {tab.toString()}
       </button>
+      {isSelected && (
+        <button className="w-10 aspect-square border-r border-y border-slate-400 rounded-r text-lg font-light">
+          +
+        </button>
+      )}
     </li>
   );
 }

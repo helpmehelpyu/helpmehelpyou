271bfb5037fdc21dee3dfd1ee1f04996277b5eed
Menghao Yu <menghaoyu2002@gmail.com>
2022-08-30 21:20:41 -0400

    Update PopupOverlay props for more reusability

--- client/components/PopupOverlay.tsx
+++ client/components/PopupOverlay.tsx
@@ -3,13 +3,17 @@ import { useEffect } from 'react';
 interface Props {
   setShowPopup: (val: boolean) => void;
   children?: JSX.Element | JSX.Element[];
+  backgroundOpacity?: number;
   stopPropagation?: boolean;
+  showCloseIcon?: boolean;
 }
 
 export default function PopupOverlay({
   setShowPopup,
   children,
+  backgroundOpacity = 90,
   stopPropagation = true,
+  showCloseIcon = true,
 }: Props) {
   useEffect(() => {
     const oldOverflowStyle = document.documentElement.style.overflow;
@@ -21,12 +25,16 @@ export default function PopupOverlay({
 
   return (
     <div
-      className="fixed w-full h-full bg-black bg-opacity-90 z-10"
+      className={
+        'fixed w-full h-full bg-black z-10' + ` bg-opacity-${backgroundOpacity}`
+      }
       onClick={() => setShowPopup(false)}
     >
-      <div className="fixed top-0 left-2 text-white text-xl md:text-3xl mt-2 ml-2 text-center duration-300 transition-colors hover:text-red-400 cursor-pointer select-none">
-        ✕
-      </div>
+      {showCloseIcon && (
+        <div className="fixed top-0 left-2 text-white text-xl md:text-3xl mt-2 ml-2 text-center duration-300 transition-colors hover:text-red-400 cursor-pointer select-none">
+          ✕
+        </div>
+      )}
       <span
         onClick={(event) => {
           if (stopPropagation) event.stopPropagation();

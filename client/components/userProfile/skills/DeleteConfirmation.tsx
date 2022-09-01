66489114a7bf160f51cfc71ac4fa23323413cce2
Menghao Yu <menghaoyu2002@gmail.com>
2022-08-31 23:01:33 -0400

    Remove close button on delete confirmation and fixed zoom issue

--- client/components/userProfile/skills/DeleteConfirmation.tsx
+++ client/components/userProfile/skills/DeleteConfirmation.tsx
@@ -26,32 +26,28 @@ export default function DeleteConfirmationPopup({
       showCloseIcon={false}
     >
       <div className="fixed drop-shadow-lg w-1/3 h-1/3 rounded p-10 space-y-5 bg-white top-0 left-0 right-0 bottom-0 m-auto overflow-auto">
-        <div
-          onClick={closePopup}
-          className="absolute top-0 left-2 text-black text-sm md:text-lg mt-2 ml-2 text-center duration-300 transition-colors hover:text-red-400 cursor-pointer select-none"
-        >
-          ✕
-        </div>
         {deleteError ? (
           <p className="text-red-600">
             The request resource could not be deleted. Try Again Later.
           </p>
         ) : (
-          <div className="flex flex-col justify-center align-center w-full h-5/6 text-center">
-            <p>Are you sure you want to delete the {deletedItemName}</p>
-            <div className="flex justify-center align-center md:gap-16 gap-5 flex-wrap w-full">
-              <button
-                className="font-bold text-cyan-600 hover:bg-gray-200 p-2 rounded"
-                onClick={handleDelete}
-              >
-                Yes
-              </button>
-              <button
-                className="font-bold text-red-600 hover:bg-gray-200 p-2 rounded"
-                onClick={closePopup}
-              >
-                No
-              </button>
+          <div className="flex flex-col w-full h-full text-center">
+            <div className="m-auto">
+              <p>Are you sure you want to delete the {deletedItemName}</p>
+              <div className="flex justify-center align-center md:gap-16 gap-5 flex-wrap w-full mt-10">
+                <button
+                  className="font-bold text-cyan-600 hover:bg-gray-200 p-2 rounded"
+                  onClick={handleDelete}
+                >
+                  Yes
+                </button>
+                <button
+                  className="font-bold text-red-600 hover:bg-gray-200 p-2 rounded"
+                  onClick={closePopup}
+                >
+                  No
+                </button>
+              </div>
             </div>
           </div>
         )}

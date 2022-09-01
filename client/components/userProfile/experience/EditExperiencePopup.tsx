87b45fb655686980fb44079404dfc7c7bf25944a
Menghao Yu <menghaoyu2002@gmail.com>
2022-08-31 22:41:32 -0400

    Fix issue where zoom in was breaking add/edit experience

--- client/components/userProfile/experience/EditExperiencePopup.tsx
+++ client/components/userProfile/experience/EditExperiencePopup.tsx
@@ -36,33 +36,33 @@ export default function EditExperiencePopup({
   return (
     <PopupOverlay setShowPopup={setShowPopup}>
       <div className="fixed p-10 left-0 right-0 top-0 bottom-0 m-auto rounded bg-white h-5/6 w-1/3 space-y-10 min-w-min overflow-auto">
-        <div className="flex flex-col w-full h-full justify-center">
-          <h1 className="mb-10 text-2xl text-center font-semibold">
-            Edit Experience
-          </h1>
-          <ExperienceForm
-            experience={experienceToEdit}
-            submitData={(data) => {
-              return axios.put(
-                'users/experience',
-                { id: experienceToEdit.id, ...data },
-                {
-                  headers: {
-                    Authorization: 'Bearer ' + getAuthCookie(),
-                  },
-                }
-              );
-            }}
-            setRefetchUserData={setRefetchUserData}
-            setShowPopup={setShowPopup}
-          ></ExperienceForm>
-          <button
-            onClick={handleDelete}
-            className="m-2 mt-8 w-20 p-1 text-red-600 border-2 rounded border-red-600  hover:bg-slate-200 hover:cursor-pointer"
-          >
-            Delete
-          </button>
-          <p className="text-red-500 text-xs mx-2 mb-2 px-1">{deleteErrors}</p>
+        <div className="flex flex-col w-full h-full">
+          <div className="m-auto">
+            <h1 className="mb-10 text-2xl text-center font-semibold">
+              Edit Experience
+            </h1>
+            <ExperienceForm
+              experience={experienceToEdit}
+              submitData={(data) => {
+                return axios.put(
+                  'users/experience',
+                  { id: experienceToEdit.id, ...data },
+                  {
+                    headers: {
+                      Authorization: 'Bearer ' + getAuthCookie(),
+                    },
+                  }
+                );
+              }}
+              setRefetchUserData={setRefetchUserData}
+              setShowPopup={setShowPopup}
+              handleDelete={handleDelete}
+            ></ExperienceForm>
+
+            <p className="text-red-500 text-xs mx-2 mb-2 px-1">
+              {deleteErrors}
+            </p>
+          </div>
         </div>
       </div>
     </PopupOverlay>

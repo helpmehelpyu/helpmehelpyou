9daf5f0e8e984548dc3c5fb1ab7b5c753894cacd
Menghao Yu <menghaoyu2002@gmail.com>
2022-08-31 22:50:30 -0400

    Fix all instances of flexbox zoom issue

--- client/components/userProfile/experience/EditExperiencePopup.tsx
+++ client/components/userProfile/experience/EditExperiencePopup.tsx
@@ -35,7 +35,7 @@ export default function EditExperiencePopup({
   };
   return (
     <PopupOverlay setShowPopup={setShowPopup}>
-      <div className="fixed p-10 left-0 right-0 top-0 bottom-0 m-auto rounded bg-white h-5/6 w-1/3 space-y-10 min-w-min overflow-auto">
+      <div className="fixed p-10 left-0 right-0 top-0 bottom-0 m-auto rounded bg-white h-5/6 w-1/3 space-y-10 min-w-min overflow-auto transparent-scrollbar">
         <div className="flex flex-col w-full h-full">
           <div className="m-auto">
             <h1 className="mb-10 text-2xl text-center font-semibold">

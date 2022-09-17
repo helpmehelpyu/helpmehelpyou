aa0687215f7a25dc40e8de091aa623b81da08d60
Menghao Yu <menghaoyu2002@gmail.com>
2022-09-17 16:36:18 -0400

    Updated item to not span whole width

--- client/components/home/homeItem.tsx
+++ client/components/home/homeItem.tsx
@@ -11,7 +11,7 @@ interface Props {
 export default function HomeItem({ media }: Props) {
   return (
     <div
-      className="border-2 space-y-3 rounded p-10 bg-white hover:border-gray-300 hover:cursor-pointer"
+      className="border-2 rounded p-10 bg-white hover:border-gray-300 hover:cursor-pointer"
       onClick={() => {
         document.location.href = '/media/' + media.id;
       }}
@@ -30,12 +30,12 @@ export default function HomeItem({ media }: Props) {
         className="rounded"
       ></Image>
       <Link href={'/media/' + media.id}>
-        <h1 className="text-5xl mt-5 font-medium hover:underline">
+        <h1 className="text-5xl mt-5 font-medium hover:underline w-fit p-2">
           {media.title}
         </h1>
       </Link>
       <div
-        className="flex rounded items-center gap-2 hover:bg-slate-100"
+        className="flex rounded items-center gap-2 hover:bg-slate-100 p-2 w-fit"
         onClick={(event) => {
           event.stopPropagation();
           document.location.href = '/users/' + media.author.id;
@@ -48,8 +48,10 @@ export default function HomeItem({ media }: Props) {
           {media.author.firstName} {media.author.lastName}
         </h2>
       </div>
-      <h2>{media.description}</h2>
-      <p>{getDateDifferentAsString(new Date(media.uploadDate))}</p>
+      <h2 className="pl-2 font-light">{media.description}</h2>
+      <p className="pl-2">
+        {getDateDifferentAsString(new Date(media.uploadDate))}
+      </p>
     </div>
   );
 }

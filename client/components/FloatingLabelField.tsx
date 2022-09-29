cb0086d305bf0da4967c36f77c59c32a17c822ab
Menghao Yu <menghaoyu2002@gmail.com>
2022-09-29 00:15:12 -0400

    Fix firefox displaying issues

--- client/components/FloatingLabelField.tsx
+++ client/components/FloatingLabelField.tsx
@@ -24,7 +24,7 @@ export default function FloatingLabelInput({
   return (
     <div className="relative">
       <input
-        className="relative rounded focus:outline-cyan-500 w-full border-2 p-1 m-2 z-10 bg-transparent peer min-w-max"
+        className="relative rounded focus:border-cyan-500 w-full border-2 outline-none p-1 m-2 z-10 bg-transparent peer min-w-max"
         type={type}
         name={placeholder.toLowerCase()}
         required={isRequired}
@@ -40,6 +40,7 @@ export default function FloatingLabelInput({
         className="
 					absolute 
 					-top-3.5
+          left-0
 					text-sm
 					my-3 
 					mx-3 
@@ -55,7 +56,8 @@ export default function FloatingLabelInput({
 					peer-focus:-top-3.5
 					peer-focus:text-sm
 					peer-focus:z-20
-          whitespace-nowrap
+          whitespace
+          select-none
 				"
       >
         {placeholder}
